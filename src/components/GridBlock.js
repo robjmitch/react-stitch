671cf9cee80cb29b6656d2767dba35bc03c64bc4
Robby Mitchell <robjmitch@gmail.com>
2018-04-12 11:41:24 -0400

    Added grid section prop to GridBlock component

--- src/components/GridBlock.js
+++ src/components/GridBlock.js
@@ -3,7 +3,10 @@ import styled from 'styled-components'
 
 const DisplayGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(12, 1fr);
+  grid-template-columns: ${props => props.gridSections ?
+    `repeat(${props.gridSections}, 1fr)` :
+    `repeat(12, 1fr)`};
+
   grid-gap: ${props => props.gridGap ? props.gridGap : 0};
 `
 
