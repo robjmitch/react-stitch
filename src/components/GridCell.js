0a0af6539476eef8902dd66ac044f21a88a00624
Robby Mitchell <robjmitch@gmail.com>
2018-04-17 13:36:51 -0400

    Changes to GridCell component to accept a gridStyles object prop

--- src/components/GridCell.js
+++ src/components/GridCell.js
@@ -2,24 +2,50 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Cell = styled.div`
-  grid-column: ${props => props.gridColumn ?
-    props.gridColumn :
-    props.gridColumnSpan ?
-      `span ${props.gridColumnSpan}` :
-      'auto'};
-  grid-row: ${props => props.gridRow ?
-    props.gridRow :
-    props.gridRowSpan ?
-      `span ${props.gridRowSpan}` :
-      'auto'};
-  align-self: ${props => props.vAlignCell ? props.vAlignCell : 'start'};
-  justify-self: ${props => props.alignCell ? props.alignCell : 'start'};
+  grid-column: ${state => state.gridColumnSpan ?
+    `span ${state.gridColumnSpan}` :
+    state.gridColumn};
+  grid-row: ${state => state.gridRowSpan ?
+    `span ${state.gridRowSpan}` :
+    state.gridRow};
+  align-self: ${state => state.vAlignCell};
+  justify-self: ${state => state.alignCell};
 `
 
 class GridCell extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      alignCell: 'start',
+      gridColumn: 'auto',
+      gridColumnSpan: 'auto',
+      gridRow: 'auto',
+      gridRowSpan: 'auto',
+      vAlignCell: 'start'
+    }
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    const updatedProps = {}
+
+    nextProps = nextProps.gridStyles ? nextProps.gridStyles : nextProps
+
+    for (let prop in nextProps) {
+      // Only update the properties that have changed and add those properties
+      // to our updatedProps object.
+      if (nextProps[prop] !== prevState[prop]) {
+        updatedProps[prop] = nextProps[prop]
+      }
+    }
+
+    if (!updatedProps) { return null }
+
+    return updatedProps
+  }
+
   render() {
     return (
-      <Cell {...this.props}>
+      <Cell className={this.props.className} {...this.state}>
         {this.props.children}
       </Cell>
     )
